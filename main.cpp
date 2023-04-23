1ab9f49c33192a7e15e6a17514aa36ce901a24b9
sw1tcherrr <sw1tcherrr@mail.ru>
2023-04-24 00:47:18 +0300

    variadic

--- main.cpp
+++ main.cpp
@@ -1,71 +1,48 @@
 #include <iostream>
-#include <array>
 #include <functional>
+#include <cassert>
 
-
-template <size_t N>
-using vec = std::array<int, N>;
-
-template <size_t N, typename F>
-concept fun = requires(F&& f, vec<N> const& x) {
-	{ std::invoke(std::forward<F>(f), x) } -> std::same_as<int>;
+constexpr auto Z = [](auto) -> int {
+	return 0;
 };
 
-constexpr int Z(auto) {
-	return 0;
-}
-
-constexpr int N(vec<1> x) {
-	return x[0] + 1;
-}
+constexpr auto N = [](int x) -> int {
+	return x + 1;
+};
 
 template <size_t K, size_t N>
-constexpr int U(vec<N> const& x) {
-	return x[K];
-}
+requires (K >= 1 && K <= N)
+constexpr auto U = [](std::same_as<int> auto... xs) -> int {
+	assert(sizeof...(xs) == N);
+	return std::get<K - 1>(std::make_tuple(xs...));
+};
 
-template <size_t K, size_t N, auto g, auto... fs>
-requires fun<K, decltype(g)> && (fun<N, decltype(fs)> &&...)
-constexpr int S(vec<N> const& x) {
-	return g({fs(x)...});
-}
+template <auto g, auto... fs>
+constexpr auto S = [](std::same_as<int> auto... xs) -> int {
+	return g(fs(xs...)...);
+};
 
-
-template <size_t N, auto f, auto g>
-requires fun<N + 2, decltype(g)> && fun<N, decltype(f)>
-constexpr int R(vec<N> const& x, int y) {
+template <auto f, auto g>
+constexpr auto R = [](int y, std::same_as<int> auto... xs) -> int {
+	// todo as for loop
 	if (y == 0) {
-		return f(x);
+		return f(xs...);
 	} else {
-		vec<N + 2> arg;
-		for (int i = 0; i < N; ++i) {
-			arg[i] = x[i];
-		}
-		arg[N] = y - 1;
-		arg[N + 1] = R<N, f, g>(x, y - 1);
-		return g(arg);
+		return g(y - 1, R<f, g>(y - 1, xs...), xs...);
 	}
-}
+};
 
 
 int main() {
-	std::cout << U<1, 3>({1, 2, 3}) << "\n";
-	std::cout << S<1, 1, N, N>({100}) << "\n";
-	std::cout << R<1, U<0, 1>, S<1, 3, N, U<2, 3>>>({5}, 10) << "\n";
+	std::cout << U<2, 3>(1, 2, 3) << "\n";
+	std::cout << S<N, N>(100) << "\n";
+	std::cout << R<U<1, 1>, S<N, U<2, 3>>>(10, 5) << "\n";
 
-	auto id = U<0, 1>;
+	auto id = U<1, 1>;
 
-	int (*add)(vec<1> const&, int) = &R<1,
-				id,
-				S<1, 3, N, U<2, 3>>>;
+	auto add = R<id, S<N, U<2, 3>>>;
 
-//	auto add_self = S<1, 1, add, id>;
-//
-//	auto mul = R<1,
-//	        	Z,
-//				add_self>;
-
-	std::cout << mul({5}, 3);
+	std::cout << add(1, 2) << "\n";
 
 	return 0;
 }
