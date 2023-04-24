884287e48dc06aab967b335350ac0b50ea2448cd
sw1tcherrr <sw1tcherrr@mail.ru>
2023-04-24 13:10:43 +0300

    ef

--- main.cpp
+++ main.cpp
@@ -13,7 +13,10 @@ constexpr auto N = [](int x) -> int {
 template <size_t K, size_t N>
 requires (K >= 1 && K <= N)
 constexpr auto U = [](std::same_as<int> auto... xs) -> int {
-	assert(sizeof...(xs) == N);
+	if (sizeof...(xs) != N) {
+		std::cerr << sizeof...(xs) << " " << N << "\n";
+		assert(false);
+	}
 	return std::get<K - 1>(std::make_tuple(xs...));
 };
 
@@ -31,18 +34,33 @@ constexpr auto R = [](int y, std::same_as<int> auto... xs) -> int {
 	return res;
 };
 
+constexpr auto id = U<1, 1>;
+
 template <auto F>
 constexpr auto flip = S<F, U<2, 2>, U<1, 2>>;
 
-template <size_t K>
-constexpr auto one = S<S<N, Z>, U<1, K>>;
+template <int A, size_t Arity>
+constexpr auto num = S<N, num<A - 1, Arity>>;
 
-template <size_t K>
-constexpr auto zero = S<Z, U<1, K>>;
+template <size_t Arity>
+constexpr auto num<0, Arity> = S<Z, U<1, Arity>>;
+
+template <size_t Arity>
+constexpr auto zero = num<0, Arity>;
+
+template <size_t Arity>
+constexpr auto one = num<1, Arity>;
+
+template <size_t Arity>
+constexpr auto two = num<1, Arity>;
+
+template <auto F, int A>
+constexpr auto proj1 = S<F, num<A, 1>, id>;
+
+template <auto F, int B>
+constexpr auto proj2 = S<F, id, num<B, 1>>;
 
 int main() {
-	auto id = U<1, 1>;
-
 	auto add = R<id, S<N, U<2, 3>>>;
 	std::cout << add(1, 2) << "\n";
 
@@ -81,6 +99,41 @@ int main() {
 	std::cout << div(5, 2) << " " << div(4, 2) << " " << div(100, 25) << " " << div(0, 5) << " " << div(5, 0) << "\n";
 	auto mod = S<sub, U<1, 2>, S<mul, U<2, 2>, S<div, U<1, 2>, U<2, 2>>>>; // a - b * (a / b)
 	std::cout << mod(5, 2) << " " << mod(4, 2) << " " << mod(121, 25) << " " << mod(0, 5) << " " << mod(5, 0) << "\n";
+
+	// 1e
+	auto sqrt = S<
+				  R<zero<1>,
+				    S<add, // res += 1 if ((yi+1) * (yi+1) <= x)
+					  U<2, 3>,
+					  S<LE,
+					    S<mul, S<N, U<1, 3>>, S<N, U<1, 3>>>,
+						U<3, 3>
+					  >
+				    >
+	        	  >,
+				  id, id
+				>;
+	std::cout << sqrt(0) << " " << sqrt(1) << " " << sqrt(4) << " " << sqrt(30) << "\n";
+
+	// 1f
+	auto pair = S<R<
+	                   S<div, // / 2
+						 S<mul, // *
+						   S<add, U<1, 2>, U<2, 2> >, // (a + b)
+						   S<add, S<add, U<1, 2>, U<2, 2> >, one<2> > // (a + b + 1)
+					     >,
+						 two<2>
+					   >,
+	        		   S<N, U<2, 4> >
+				     >,
+				   U<2, 2>, U<1, 2>, U<2, 2>
+				 >;
+	std::cout << pair(0, 0) << " " << pair(1, 1) << " " << pair(1, 3) << " " << pair(3, 2) << "\n";
+
+	for (int i = 0; i < 5; ++i) {
+		std::cout << proj1<pair, 1>(i) << " ";
+	}
+	std::cout << "\n";
 
 	return 0;
 }
